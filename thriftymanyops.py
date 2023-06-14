ca62f8ba71a279f226e72aa2d1681f31fa1f75f6
Vincent Gripon <vincent@gripon.fr>
2023-06-14 22:58:04 +0200

    reduced flops

--- thriftymanyops.py
+++ thriftymanyops.py
@@ -5,72 +5,36 @@ class Thrifty(nn.Module):
     def __init__(self, hidden_dim, depth, downsampling, num_classes):
         super().__init__()
 
+        self.embed = torch.nn.Sequential(
+            nn.Conv2d(3, hidden_dim, 3, padding = 1, bias = False),
+            nn.BatchNorm2d(hidden_dim),
+            nn.ReLU()
+        )
+
+
         self.conv1 = nn.Conv2d(hidden_dim, hidden_dim, 3, padding = 1, bias = False)
         self.conv2 = nn.Conv2d(hidden_dim, hidden_dim, 3, padding = 1, bias = False)
+
+        self.bn1 = nn.ModuleList([nn.BatchNorm2d(hidden_dim) for _ in range(depth)])
+        self.bn2 = nn.ModuleList([nn.BatchNorm2d(hidden_dim) for _ in range(depth)])
 
         self.classifier = nn.Linear(hidden_dim, num_classes)
         self.depth = depth
         self.downsampling = downsampling
 
-        self.reduction = 1
-        for i in downsampling:
-            self.reduction *= 2
-
-        bn1 = []
-        bn2 = []
-        subfilters1 = []
-        subfilters2 = []
-        reduction = self.reduction
-        for i in range(depth):
-            if i in self.downsampling:
-                reduction = reduction // 2
-            bn1.append(nn.BatchNorm2d(hidden_dim // reduction))
-            bn2.append(nn.BatchNorm2d(hidden_dim // reduction))
-            subfilters1.append(torch.randperm(hidden_dim)[:hidden_dim // reduction])
-            subfilters2.append(torch.randperm(hidden_dim)[:hidden_dim // reduction])
-
-            
-        self.bn1 = nn.ModuleList(bn1)
-        self.bn2 = nn.ModuleList(bn2)
-        self.subfilters1 = subfilters1
-        self.subfilters2 = subfilters2
-            
-        self.hidden_dim = hidden_dim
-
-        self.embed = torch.nn.Sequential(
-            nn.Conv2d(3, hidden_dim // self.reduction, 3, padding = 1, bias = False),
-            nn.BatchNorm2d(hidden_dim // self.reduction),
-            nn.ReLU()
-        )
-
-
     def forward(self, x):
         y = self.embed(x)
 
-        reduction = self.reduction
-        
         for i in range(self.depth):
-#            z = self.conv1(y)
-            if i in self.downsampling:
-                new_reduction = reduction // 2
-            else:
-                new_reduction = reduction
-            convweight1 = self.conv1.weight[self.subfilters1[i]].permute(1,0,2,3)[self.subfilters1[i]].permute(1,0,2,3)
-            z = torch.nn.functional.conv2d(y, weight=convweight1[:,:self.hidden_dim // reduction], padding=1)
+            z = self.conv1(y)
             z = self.bn1[i](z)
             z = torch.relu(z)
-#            z = self.conv2(z)
-            convweight2 = self.conv2.weight[self.subfilters2[i]].permute(1,0,2,3)[self.subfilters2[i]].permute(1,0,2,3)
-            z = torch.nn.functional.conv2d(z, weight=convweight2, padding=1)
+            z = self.conv2(z)
             z = self.bn2[i](z)
-            if i in self.downsampling:
-                size = y.shape[1]
-                y = torch.nn.functional.pad(y, (0, 0, 0, 0, 0, size))
             y = y + z
             y = torch.relu(y)
             if i+1 in self.downsampling:
                 y = nn.MaxPool2d(2,2)(y)
-            reduction = new_reduction
 
         y = y.mean(-1).mean(-1)
 
